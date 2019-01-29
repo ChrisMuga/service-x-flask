3346fae704fa6348aa84cb2df0ed17f9ca06578e
Chris Muga <chrismuga94@gmail.com>
2019-01-29 15:11:06 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+@app.route('/login')
+def login():
+    return 'Login Here...'
+@app.route('/profile/<int:id>')
+def profile(id):
+    return str(id)
