6145c443c65f1306192ed4123767de04bdfe49fb
Chris Muga <chrismuga94@gmail.com>
2019-01-30 00:10:31 +0300

    insert into

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import logging
+import json
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:root@localhost/servicex'
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+
+        id              = db.Column(db.Integer, primary_key=True)
+        first_name      = db.Column(db.String(250), unique=True, nullable=False)
+        last_name       = db.Column(db.String(250), unique=True, nullable=False)
+        email_address   = db.Column(db.String(250), unique=True, nullable=False)
+
+
+        def __repr__(self):
+            return '<User %r>' % self.email_address
 
 @app.route('/')
 def hello_world():
@@ -16,3 +35,32 @@ def login():
 @app.route('/profile/<int:id>')
 def profile(id):
     return str(id)
+
+@app.route('/connect')
+def connect():
+    
+    users = Users(
+                    id              =   '1253456', 
+                    first_name      =   'Tony', 
+                    last_name       =   'Momtana', 
+                    email_address   =   'tony@mail.com' 
+                )
+    db.session.add(users)
+    db.session.commit()
+    return 'DB'
+
+@app.route('/fetch')
+def fetch():
+    x = Users.query.all()
+
+    #logging
+    logging.info('done')
+    return len(str(x))
+
+    
+    
+    
+    
+
+
+    
