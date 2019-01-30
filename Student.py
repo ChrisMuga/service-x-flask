d1a286fcb662bf46a65d4e8d7d505c7edb794416
Chris Muga <chrismuga94@gmail.com>
2019-01-30 12:03:22 +0300

    start working with classes from different files

---
+++ Student.py
@@ -0,0 +1,10 @@
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+
+    def details(self):
+        print(self.name)
+        
+# student = Student('Chris')
+# student.details()
---
+++ x.py
@@ -0,0 +1,4 @@
+from Student import Student
+
+student = Student('Muga')
+student.details()
